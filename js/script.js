b15047f2ff54b6b07f57f7f5be29aa312d106eb5
vitor-felipee <vitor07.felipe@gmail.com>
2026-02-26 09:29:18 -0300

    Accordion tranformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -11,6 +11,10 @@ import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
