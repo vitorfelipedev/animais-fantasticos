814b18d29fb73f9cbbd88b1476520dca12985420
vitor-felipee <vitor07.felipe@gmail.com>
2026-02-27 10:32:16 -0300

    refatorando fetch bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,18 +1,12 @@
-export default function initFetchBitcoin() {
-  async function fetchBitcoin(url) {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const bitcoinReal = data.BRL.buy;
-      const doacao = (100 / bitcoinReal).toFixed(8).replace('.', ',');
-      const dadosContato = document.querySelector('#contato .dados');
-      const liBitcoin = document.createElement('li');
-      liBitcoin.innerHTML = `Doe <span class="verde-destaque">${doacao}</span> bitcoin para nos ajudar!`;
-      dadosContato.appendChild(liBitcoin);
-    } catch (error) {
-      console.error(error);
-    }
+export default async function fetchBitcoin(url, target) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const bitcoinReal = data.BRL.buy;
+    const doacao = (100 / bitcoinReal).toFixed(8).replace('.', ',');
+    const btcPreco = document.querySelector(target);
+    btcPreco.innerText = doacao;
+  } catch (error) {
+    console.error(error);
   }
-
-  fetchBitcoin('https://blockchain.info/ticker');
 }
