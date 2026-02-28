338c2985ce6ea3a1fc1d84278069bc86eae326ee
vitor-felipee <vitor07.felipe@gmail.com>
2026-02-28 01:09:13 -0300

    Adicionando debounce a animacao-scroll

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,8 +1,9 @@
+import debounce from './debounce.js';
 export default class AnimacaoScroll {
   constructor(section) {
     this.sections = document.querySelectorAll(section);
     this.windowMetade = window.innerHeight * 0.6;
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   //Obtém a distância de cada item em relação ao topo
